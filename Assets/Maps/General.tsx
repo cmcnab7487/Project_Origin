71f3a81387e8f5078378d21c0e1958a9accb269f
cmcnab7487 <36284992+cmcnab7487@users.noreply.github.com>
2018-03-03 17:35:07 -0500

    First tilemap, added a player sprite

---
+++ Assets/Maps/General.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="General" tilewidth="16" tileheight="16" tilecount="2916" columns="54">
+ <image source="../Sprites/General.png" width="864" height="864"/>
+</tileset>
---
+++ Assets/Scripts/title_handler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class title_handler : MonoBehaviour
+{
+    public void loadNextScene(string sceneName)
+    {
+        if (sceneName == "Options")
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        else
+            SceneManager.LoadScene(sceneName);
+    }
+}
